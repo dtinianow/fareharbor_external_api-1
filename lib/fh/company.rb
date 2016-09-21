90d5273aa5cbda502bfc04588373d57945dfbb6a
Zack Forbing <zackforbing@gmail.com>
2016-09-21 14:30:02 -0600

    refactor redundancies into private methods

--- lib/fh/company.rb
+++ lib/fh/company.rb
@@ -7,10 +7,6 @@ module FH
       @shortname = company[:shortname]
     end
 
-    def service
-      FareHarborService.new
-    end
-
     def items
       items = service.get_items(shortname)
       items[:items].map { |item| FH::Company::Item.new(item) }
@@ -18,12 +14,12 @@ module FH
 
     def availabilities_by_date(availability_data)
       availabilities = service.get_availabilities_by_date(shortname, availability_data)
-      availabilities[:availabilities].map { |availability| FH::Company::Availability.new(availability) }
+      build_availabilities(availabilities)
     end
 
     def availabilities_by_date_range(availability_data)
       availabilities = service.get_availabilities_by_date_range(shortname, availability_data)
-      availabilities[:availabilities].map { |availability| FH::Company::Availability.new(availability) }
+      build_availabilities(availabilities)
     end
 
     def availability(pk)
@@ -38,12 +34,12 @@ module FH
 
     def lodgings
       lodgings = service.get_lodgings(shortname)
-      create_lodgings(lodgings)
+      build_lodgings(lodgings)
     end
 
     def availability_lodgings(pk)
       lodgings = service.get_availability_lodgings(shortname, pk)
-      create_lodgings(lodgings)
+      build_lodgings(lodgings)
     end
 
     def create_booking(booking_request)
@@ -60,11 +56,19 @@ module FH
       cancelled_booking = service.delete_booking(shortname, uuid)
       FH::Company::Booking.new(cancelled_booking[:booking])
     end
-  end
 
-  private
+    private
 
-    def create_lodgings(lodgings)
-      lodgings[:lodgings].map { |lodging| FH::Company::Lodging.new(lodging) }
+      def service
+        FareHarborService.new
+      end
+
+      def build_lodgings(lodgings)
+        lodgings[:lodgings].map { |lodging| FH::Company::Lodging.new(lodging) }
+      end
+
+      def build_availabilities(availabilities)
+        availabilities[:availabilities].map { |availability| FH::Company::Availability.new(availability) }
+      end
     end
 end
